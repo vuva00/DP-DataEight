e7772adbd8a2482d1e2fca0822eb74ddf08d8f3e
Junacik <filip.slysko@gmail.com>
2025-06-08 21:57:08 +0200

    Added folder final_GBQ_scripts with the scripts and logic I used in the GBQ

---
+++ final_GBQ_scripts/run-train-job-monthly.py
@@ -0,0 +1,29 @@
+#Example script to run a custom job on Google Cloud Vertex AI
+# This script initializes the Vertex AI environment and runs a custom job for training an XGBoost model monthly.
+
+from google.cloud import aiplatform
+
+aiplatform.init(
+    project="behavio-test",
+    location="europe-west1",
+    staging_bucket="gs://withdraws-model"
+)
+
+job = aiplatform.CustomJob(
+    display_name="xgb-model-monthly-job",
+    worker_pool_specs=[
+        {
+            "machine_spec": {
+                "machine_type": "n1-standard-4",
+            },
+            "replica_count": 1,
+            "container_spec": {
+                "image_uri": "europe-docker.pkg.dev/behavio-test/vertex-containers/xgb-model:latest",
+                "command": ["python3"],
+                "args": ["/app/model_train_run_monthly.py"],
+            },
+        }
+    ],
+)
+
+job.run(sync=True)
